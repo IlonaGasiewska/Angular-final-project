d5f4f4a95cac6d9ceb52d12c901719fd8e1d4945
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2024-01-21 15:12:18 +0100

    added game history list

--- tetris-game/src/app/user-action-history/user-action-history.component.ts
+++ tetris-game/src/app/user-action-history/user-action-history.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-user-action-history',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './user-action-history.component.html',
   styleUrl: './user-action-history.component.scss'
 })
 export class UserActionHistoryComponent {
+  gameHistory = ["button left", "button right"]
 }
