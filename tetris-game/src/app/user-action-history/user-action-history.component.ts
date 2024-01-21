503a46699e8607d5bda1f9b7d0f71708eb4e1e10
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2024-01-21 16:57:57 +0100

    added function to show selected actions

--- tetris-game/src/app/user-action-history/user-action-history.component.ts
+++ tetris-game/src/app/user-action-history/user-action-history.component.ts
@@ -1,13 +1,24 @@
 import { NgForOf } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-action-history',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, FormsModule],
   templateUrl: './user-action-history.component.html',
   styleUrl: './user-action-history.component.scss'
 })
 export class UserActionHistoryComponent {
-  gameHistory = ["button left", "button right"]
+  gameHistory = ["start game", "start game", "button left", "button right" ,"pause", "pause","rotate", "down", "drop", "start game", "start game", "button left", "button right" ,"pause", "pause","rotate", "down", "drop"];
+  
+  selectedAction: string = "all";
+
+  get filteredGameHistory() {
+    if (this.selectedAction === "all") {
+      return this.gameHistory;
+    } else {
+      return this.gameHistory.filter((e) => e.includes(this.selectedAction));
+    }
+  }
 }
